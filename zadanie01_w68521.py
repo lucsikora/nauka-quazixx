69bfa9b751bc0a121d47961957810a782dbff15a
Lucas Sikora <4frozz@gmail.com>
2022-10-31 15:13:57 +0100

    Code improvement

--- zadanie01_w68521.py
+++ zadanie01_w68521.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python3
-def zadanie():
-    print("Zadanie domowe w68521")
+def zadanie01():
+    # Funkcja wypisuje dowolny komunikat
+    message = "Zadanie domowe w68521"
+    return message
 
-def zadanie1a(tresc):
-    print("podane słowo to "+ tresc)
+def zadanie01a(tresc):
+    # Funkcja wypisuje przekazany jej komunikat
+    if tresc == '': 
+        message = "Nie wpisano treści"
+    else:
+        message = "Zadanie 01a. Wypisywanie podanej treści: " + tresc
+    return message
 
-
-
-def zadanie2(Liczba1, liczba2):
+def zadanie02(liczba1, liczba2):
+     # Opisac ladnie co robi ta funkcja, ale najlepiej po angielsku
     wynik = l1+l2 /2
     return wynik
 
-l1 = int(input("podaj 1 liczbę "))
-l2 = int(input("podaj 2 liczbę "))
 
-print("średni liczba wynosi", zadanie2(l1,l2))
-t = input("podaj dowolne słowo: ")
+# OUTPUT
+print("==== Zadanie 01 - Wypisz komunikat")
+print(zadanie01())
 
-zadanie()
-zadanie1a(t)
-zadanie2(l1,l2)
+print("==== Zadanie 01a - Wypisz podany przez użytkownika komunikat")
+# Pewnie pasowało by tu sprawdzać czy ktoś wpisał tekst, jeśli nie, to zeby prosiło go jeszcze raz o wpisanie.
+t = input("Podaj dowolne słowo: ")
+print(zadanie01a(t))
+
+print("==== Zadanie 02 - Oblicz średnią z dwóch liczb naturalnych")
+# Brakuje ci tu funkcji sprawdzajacych czy ktos nie wprowadzi np. stringa 
+l1 = int(input("Podaj 1 liczbę "))
+l2 = int(input("Podaj 2 liczbę "))
+print("średni liczba wynosi", zadanie02(l1,l2))
