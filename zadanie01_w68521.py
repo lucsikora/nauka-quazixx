83cd8d58863a843e4ebc791e08caf51969c2fc2d
Lucas Sikora <4frozz@gmail.com>
2022-10-31 14:35:30 +0100

    Initial Commit

---
+++ zadanie01_w68521.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+def zadanie():
+    print("Zadanie domowe w68521")
+
+def zadanie1a(tresc):
+    print("podane słowo to "+ tresc)
+
+
+
+def zadanie2(Liczba1, liczba2):
+    wynik = l1+l2 /2
+    return wynik
+
+l1 = int(input("podaj 1 liczbę "))
+l2 = int(input("podaj 2 liczbę "))
+
+print("średni liczba wynosi", zadanie2(l1,l2))
+t = input("podaj dowolne słowo: ")
+
+zadanie()
+zadanie1a(t)
+zadanie2(l1,l2)
